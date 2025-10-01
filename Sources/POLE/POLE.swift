71ff0da72f1ff6ca2a5cb4e223d14fc4bc7df00a
rayman-zhao <zhao_yu@me.com>
2025-10-01 20:03:28 +0800

    Remove unnecessary namespace enum

--- Sources/POLE/POLE.swift
+++ Sources/POLE/POLE.swift
@@ -1,86 +1,84 @@
 import CPOLE
 import Foundation
 
-public enum POLE {
-    public class Storage {
-        public enum Result : Int32 {
-            case ok
-            case openFailed
-            case notOLE
-            case badOLE
-            case unknownError
-        }
-
-        fileprivate let cstorage: UnsafeMutableRawPointer
-
-        public init(_ path: String) {
-            cstorage = CStorage_create(path)
-        }
-
-        deinit {
-            CStorage_destroy(cstorage)
-        }
-
-        public func open() -> Bool {
-            return CStorage_open(cstorage)
-        }
-
-        public func close() {
-            CStorage_close(cstorage)
-        }
-
-        public func result() -> Result {
-            return Result(rawValue: CStorage_result(cstorage))!
-        }
-
-        public func entries(_ path: String = "/") -> [String] {
-            return consumeCStrings2SwiftStrings(CStorage_entries(cstorage, path))
-        }
-
-        public func isDirectory(_ name: String) -> Bool {
-            return CStorage_isDirectory(cstorage, name);
-        }
-
-        public func exists(_ name: String) -> Bool {
-            return CStorage_exists(cstorage, name);
-        }
-
-        public func getAllStreams(_ storageName: String) -> [String] {
-            return consumeCStrings2SwiftStrings(CStorage_getAllStreams(cstorage, storageName))
-        }
+public class Storage {
+    public enum Result : Int32 {
+        case ok
+        case openFailed
+        case notOLE
+        case badOLE
+        case unknownError
     }
 
-    public class Stream {
-        private let cstream: UnsafeMutableRawPointer
+    fileprivate let cstorage: UnsafeMutableRawPointer
 
-        public init(_ storage: Storage, _ name: String) {
-            cstream = CStream_create(storage.cstorage, name)
-        }
+    public init(_ path: String) {
+        cstorage = CStorage_create(path)
+    }
 
-        deinit {
-            CStream_destroy(cstream)
-        }
+    deinit {
+        CStorage_destroy(cstorage)
+    }
 
-        public func fullName() -> String {
-            return consumeCString2SwiftString(CStream_fullName(cstream))
-        }
+    public func open() -> Bool {
+        return CStorage_open(cstorage)
+    }
 
-        public func size() -> Int {
-            return CStream_size(cstream)
-        }
+    public func close() {
+        CStorage_close(cstorage)
+    }
 
-        public func read(_ maxlen: Int = Int.max) -> Data {
-            let bufSize = min(size(), maxlen)
-            guard bufSize > 0 else { return Data() }
+    public func result() -> Result {
+        return Result(rawValue: CStorage_result(cstorage))!
+    }
 
-            var buf = [UInt8](repeating: 0, count: bufSize)
-            let readSize = CStream_read(cstream, &buf, bufSize)
-            return Data(buf[..<readSize])
-        }
+    public func entries(_ path: String = "/") -> [String] {
+        return consumeCStrings2SwiftStrings(CStorage_entries(cstorage, path))
+    }
 
-        public func fail() -> Bool {
-            return CStream_fail(cstream)
-        }
+    public func isDirectory(_ name: String) -> Bool {
+        return CStorage_isDirectory(cstorage, name);
+    }
+
+    public func exists(_ name: String) -> Bool {
+        return CStorage_exists(cstorage, name);
+    }
+
+    public func getAllStreams(_ storageName: String) -> [String] {
+        return consumeCStrings2SwiftStrings(CStorage_getAllStreams(cstorage, storageName))
+    }
+}
+
+public class Stream {
+    private let cstream: UnsafeMutableRawPointer
+
+    public init(_ storage: Storage, _ name: String) {
+        cstream = CStream_create(storage.cstorage, name)
+    }
+
+    deinit {
+        CStream_destroy(cstream)
+    }
+
+    public func fullName() -> String {
+        return consumeCString2SwiftString(CStream_fullName(cstream))
+    }
+
+    public func size() -> Int {
+        return CStream_size(cstream)
+    }
+
+    public func read(_ maxlen: Int = Int.max) -> Data {
+        let bufSize = min(size(), maxlen)
+        guard bufSize > 0 else { return Data() }
+
+        var buf = [UInt8](repeating: 0, count: bufSize)
+        let readSize = CStream_read(cstream, &buf, bufSize)
+        return Data(buf[..<readSize])
+    }
+
+    public func fail() -> Bool {
+        return CStream_fail(cstream)
     }
 }
 
--- Tests/RsPackTests/POLETests.swift
+++ Tests/RsPackTests/POLETests.swift
@@ -8,7 +8,7 @@ func testPOLE() throws {
     let path = Bundle.module.path(forResource: "Workbook.xls")
     try #require(path != nil, "No xls file found in \(Bundle.module.resourcePath!))")
     
-    let s = POLE.Storage(path!)
+    let s = Storage(path!)
     let ret = s.open()
     #expect(ret)
     #expect(s.result() == .ok)
@@ -25,7 +25,7 @@ func testPOLE() throws {
     let streams = s.getAllStreams("/")
     #expect(streams.count == e.count)
 
-    let stream = POLE.Stream(s, "/Workbook")
+    let stream = Stream(s, "/Workbook")
     #expect(stream.fullName() == "/Workbook")
     #expect(stream.size() > 0)
     #expect(stream.read(1).count == 1)
